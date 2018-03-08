d5af4e1208c6ba1ea41d12d0aa4a8c5a13dec8c8
somyasrivastava99 <say.somyasrivastava@gmail.com>
2018-03-08 17:51:45 +0530

    ear with '#' will be ignored, and an empty message aborts the commit.

---
+++ encryption.py
@@ -0,0 +1,14 @@
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = 3
+newmessage = ''
+
+message = input('Please enter a message: ')
+
+for character in message:
+	if character in alphabet:
+		position = alphabet.find(character)
+		newposition = (position + key) %26
+		newcharacter = alphabet[newposition]
+		newmessage += newcharacter
+	else:
+		newmessage += characte
